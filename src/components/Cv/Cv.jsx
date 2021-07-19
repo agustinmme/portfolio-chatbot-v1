05b1fbdb8d62b4664452ad57afbd2411491ee6bb
agustinmme <76716674+agustinmme@users.noreply.github.com>
2021-07-19 15:13:27 -0300

    fix cv
    fix cv

--- src/components/Cv/Cv.jsx
+++ src/components/Cv/Cv.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./Cv.css"
 export default function Cv() {
     return (
-            <a className='container-cv' href={"./documents/CV.pdf"} target="_blank" rel="noopener noreferrer"><img src={"./images/image.png"}  alt='pdf,cv,agustin'/></a>
+            <a className='container-cv' href={"./docs/CV.pdf"} target="_blank" rel="noopener noreferrer"><img src={"./images/image.png"}  alt='pdf,cv,agustin'/></a>
     )
 }
 
