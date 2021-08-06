1a19d820004bb851d9ef35ecbe2206372a259196
agustinmme <76716674+agustinmme@users.noreply.github.com>
2021-08-05 23:16:43 -0300

    exchange

---
+++ precache-manifest.2a2404e0b9eb871ca1057d5fb6a5492d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c785f2b5d6a82943b0f652a0e64e9d19",
+    "url": "./index.html"
+  },
+  {
+    "revision": "df2c4d41842a07d8cf9f",
+    "url": "./static/css/main.bfe3e523.chunk.css"
+  },
+  {
+    "revision": "84beed62e38c45cd57ae",
+    "url": "./static/js/2.400f41c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.400f41c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df2c4d41842a07d8cf9f",
+    "url": "./static/js/main.9bb84368.chunk.js"
+  },
+  {
+    "revision": "d7a36a90e985a1ec789c",
+    "url": "./static/js/runtime-main.538f41bb.js"
+  }
+]);
---
+++ precache-manifest.4a98a16a18b6e80c86d32f8eab2da693.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc82c8880f4101620b46cbc4d603a475",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dcbb4d975b1765f6c215",
+    "url": "./static/css/main.b35628d7.chunk.css"
+  },
+  {
+    "revision": "56ee04c062fc06c9d3e0",
+    "url": "./static/js/2.0baae63f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0baae63f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcbb4d975b1765f6c215",
+    "url": "./static/js/main.0bec011f.chunk.js"
+  },
+  {
+    "revision": "d7a36a90e985a1ec789c",
+    "url": "./static/js/runtime-main.538f41bb.js"
+  }
+]);
