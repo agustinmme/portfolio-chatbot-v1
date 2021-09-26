75928de32c5ff472eb4b031d87961104ca69edc6
agustinmme <76716674+agustinmme@users.noreply.github.com>
2021-09-26 12:04:11 -0300

    change:datajson

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.49987d2afe824e220da3f31547eaf832.js"
+  "./precache-manifest.0411b74313203ff8881c09617d1b7e45.js"
 );
 
 self.addEventListener('message', (event) => {
